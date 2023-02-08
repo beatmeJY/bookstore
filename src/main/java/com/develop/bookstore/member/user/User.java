e3210546f0b981942b3be163307086aab515deb5
beatmeJY <beatmeJY@naver.com>
2023-02-08 10:12:50 +0900

    [feat] 회원 정보 저장 로직 개발 완료.

--- src/main/java/com/develop/bookstore/member/user/User.java
+++ src/main/java/com/develop/bookstore/member/user/User.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -37,14 +38,17 @@ public class User extends DefaultEntity {
 
     // 생년
     @Column(nullable = false)
+    @Getter(AccessLevel.PRIVATE)
     private Integer birthYear;
 
     // 생월
     @Column(nullable = false)
+    @Getter(AccessLevel.PRIVATE)
     private Integer birthMonth;
 
     // 생일
     @Column(nullable = false)
+    @Getter(AccessLevel.PRIVATE)
     private Integer birthDay;
 
     // 이메일
@@ -57,14 +61,18 @@ public class User extends DefaultEntity {
 
     // 전화번호 처음
     @Column
+    @Getter(AccessLevel.PRIVATE)
     private String phoneNumber1;
 
     // 전화번호 중간
     @Column
+    @Getter(AccessLevel.PRIVATE)
+
     private String phoneNumber2;
 
     // 전화번호 마지막
     @Column
+    @Getter(AccessLevel.PRIVATE)
     private String phoneNumber3;
 
     // 로그인 플랫폼
@@ -97,12 +105,51 @@ public class User extends DefaultEntity {
         setBirthDay(birthDay);
     }
 
+    // 이후 생일은 변경 불가.
+    private void setBirthDay(Integer birthDay) {
+        if (ObjectUtils.isEmpty(birthDay)) throw new NotFormatMatchException("생년월일이 존재 하지 않습니다.");
+
+        String stringBirthDay = birthDay.toString();
+        if (stringBirthDay.length() != 8) throw new NotFormatMatchException("생년월일이 정보가 올바르지 않습니다.");
+
+        LocalDate birthDayLocal = LocalDate.now();
+        birthDayLocal = birthDayLocal.withYear(Integer.parseInt(stringBirthDay.substring(0, 4)));
+        birthDayLocal = birthDayLocal.withMonth(Integer.parseInt(stringBirthDay.substring(4, 6)));
+        birthDayLocal = birthDayLocal.withDayOfMonth(Integer.parseInt(stringBirthDay.substring(6)));
+
+        if (LocalDate.now().isBefore(birthDayLocal)) {
+            throw new NotFormatMatchException("아직 태어나지 않은 사람 입니다.");
+        }
+
+        this.birthYear = birthDayLocal.getYear();
+        this.birthMonth = birthDayLocal.getMonthValue();
+        this.birthDay = birthDayLocal.getDayOfMonth();
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if (StringUtils.hasText(phoneNumber)) {
+            String[] phoneNumSplit = phoneNumber.split("-");
+            for (String s : phoneNumSplit) {
+                try {
+                    Integer.parseInt(s);
+                } catch (NumberFormatException e) {
+                    throw new NotFormatMatchException("전화번호 양식이 올바르지 않습니다.");
+                }
+            }
+
+            if (phoneNumSplit.length != 3) {
+                throw new NotFormatMatchException("전화번호 양식이 올바르지 않습니다.");
+            }
+            this.phoneNumber1 = phoneNumSplit[0];
+            this.phoneNumber2 = phoneNumSplit[1];
+            this.phoneNumber3 = phoneNumSplit[2];
+        }
+    }
 
     public int getAge() {
         LocalDate localDate = LocalDate.now();
         int result = localDate.getYear() - getBirthYear();
 
-        // TODO - 이거 테스트 코드 만들기.
         // 23년 6월부터는 만나이로 계산하기.
         if (localDate.getYear() >= 2023 && localDate.getMonthValue() >= 6) {
             if (this.getBirthMonth() > localDate.getMonthValue() || (this.getBirthMonth() == localDate.getMonthValue() && this.getBirthDay() > localDate.getDayOfMonth())) {
@@ -114,33 +161,9 @@ public class User extends DefaultEntity {
     }
 
     public String getPhoneNumber() {
-        if (ObjectUtils.isEmpty(phoneNumber1) || ObjectUtils.isEmpty(phoneNumber2) || ObjectUtils.isEmpty(phoneNumber3)) {
+        if (!ObjectUtils.isEmpty(phoneNumber1) && !ObjectUtils.isEmpty(phoneNumber2) && !ObjectUtils.isEmpty(phoneNumber3)) {
             return phoneNumber1 + "-" + phoneNumber2 + "-" + phoneNumber3;
         }
         return null;
     }
-
-    public void setBirthDay(Integer birthDay) {
-        if (ObjectUtils.isEmpty(birthDay)) throw new NotFormatMatchException("생년월일이 존재 하지 않습니다.");
-
-        String stringBirthDay = birthDay.toString();
-        if (stringBirthDay.length() != 8) throw new NotFormatMatchException("생년월일이 정보가 올바르지 않습니다.");
-
-        this.birthYear = Integer.parseInt(stringBirthDay.substring(0, 4));
-        this.birthMonth = Integer.parseInt(stringBirthDay.substring(4, 6));
-        this.birthDay = Integer.parseInt(stringBirthDay.substring(6));
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        if (StringUtils.hasText(phoneNumber)) {
-            String[] phoneNumSplit = phoneNumber.split("-");
-            if (phoneNumSplit.length == 3) {
-                this.phoneNumber1 = phoneNumSplit[0];
-                this.phoneNumber2 = phoneNumSplit[1];
-                this.phoneNumber3 = phoneNumSplit[2];
-                throw new NotFormatMatchException("전화번호 양식이 올바르지 않습니다.");
-            }
-        }
-    }
-
 }
